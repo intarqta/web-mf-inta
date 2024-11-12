3f77d1a11b97cc57dc5b2d8dcb6b3131c3a30162
intarqta <105185644+intarqta@users.noreply.github.com>
2024-11-12 16:37:25 -0300

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,9 @@ from django.urls import path, include
 from rest_framework import routers
 from api import views
 from .views import NDVIAPIView
+from rest_framework.documentation import include_docs_urls
+# from .views import PastureAvailabilityAPIView
+
 
 rourter = routers.DefaultRouter()
 rourter.register('api', views.TaskView, 'api')
@@ -10,4 +13,5 @@ rourter.register('api', views.TaskView, 'api')
 urlpatterns = [
     path("api/v1/", include(rourter.urls)),
     path('ndvi/', NDVIAPIView.as_view(), name='ndvi'),
+    # path('disponibilidad/', PastureAvailabilityAPIView.as_view(), name='pasto-disponibilidad'),
 ]
