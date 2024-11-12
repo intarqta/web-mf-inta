b5b76068b57e9ee5e64f8cb70d20f2dd320170f9
intarqta <105185644+intarqta@users.noreply.github.com>
2024-11-12 16:36:16 -0300

    Update views.py

--- api/views.py
+++ api/views.py
@@ -4,13 +4,11 @@ from .serializer import TaskSerializer, PolygonSerializer
 from .models import Task
 from rest_framework.response import Response
 from rest_framework import status
-from .ndvi_script import get_ndvi
 import geojson
+from .evaluate import get_ndvi_and_regions
+from .ndvi_script import get_ndvi, calculate_centroid, get_nasa_power_data
+import json
 
-# Con una sola clase podemos generar el CRUD de forma automática
-class TaskView(viewsets.ModelViewSet):
-    serializer_class = TaskSerializer
-    queryset = Task.objects.all()
 
 class NDVIAPIView(APIView):
     def post(self, request):
@@ -19,29 +17,24 @@ class NDVIAPIView(APIView):
         if serializer.is_valid():
             coordinates = serializer.validated_data['coordinates']
             polygon = geojson.Polygon([coordinates])
-
-            # Obtener parámetros adicionales desde el request
             start_date = request.data.get('start_date', '2024-01-01')
             end_date = request.data.get('end_date', '2024-09-30')
-            recurso_forrajero = request.data.get('recurso_forrajero', None)
-            presencia_leñosas = request.data.get('presencia_leñosas', False)
-            porcentaje_leñosas = request.data.get('porcentaje_leñosas', 0)
+            
+            # Llamar a la función get_ndvi para obtener los datos de NDVI
+            ndvi_data = get_ndvi_and_regions(polygon)
+            print(ndvi_data)
 
-            # Asegúrate de convertir 'presencia_leñosas' a booleano si viene como string
-            if isinstance(presencia_leñosas, str):
-                presencia_leñosas = presencia_leñosas.lower() == 'true'
+            # Calcular el centroide del polígono
+            centroid = calculate_centroid(polygon)
 
-            # Llamar a la función get_ndvi con los nuevos parámetros
-            ndvi_data = get_ndvi(
-                polygon=polygon,
-                start_date=start_date,
-                end_date=end_date,
-                recurso_forrajero=recurso_forrajero,
-                presencia_leñosas=presencia_leñosas,
-                porcentaje_leñosas=porcentaje_leñosas
-            )
+            # Obtener datos de NASA POWER (temperatura y radiación)
+            nasa_power_data = get_nasa_power_data(centroid, start_date, end_date)
 
-            # Responder con los datos NDVI generados
-            return Response(ndvi_data, status=status.HTTP_200_OK)
-        
+            # Combinar los resultados de NDVI y NASA POWER
+            combined_results = {
+                "ndvi_data": ndvi_data,
+                "nasa_power_data": nasa_power_data
+            }
+
+            return Response(combined_results, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
